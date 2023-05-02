65b43d3c42a4fc0f01cc330c4ad225e4fc3ec9d2
Levon Hovhannisyan <lehovhan@microsoft.com>
2023-05-02 20:51:31 +0400

    Add project files.

---
+++ AdoNetTest1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Data.SqlClient;
+
+namespace AdoNetTest1
+{
+    internal class Program
+    {
+        const string connectionString = "Data Source=localhost;Initial Catalog=BookStoreDB;Integrated Security=True";
+
+        static void Main(string[] args)
+        {
+            string sqlExpression = "SELECT Books.Name as BooksName, Books.Year as Year, Publisher.Name as PublisherName FROM Books ";
+
+            sqlExpression += "Inner Join Publisher On Books.PublisherID = Publisher.Id";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                SqlCommand cmd = new SqlCommand(sqlExpression, connection);
+
+                using (SqlDataReader reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        Console.WriteLine($"'{reader["BooksName"]}'; {reader["PublisherName"]}; {reader["Year"]}");
+                    }
+                }
+            }
+        }
+    }
+}
